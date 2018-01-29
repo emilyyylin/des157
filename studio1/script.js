5e6c97433d3337781ce85efcc95327640c4ac578
emilyyylin <emwlin@ucdavis.edu>
2018-01-28 17:46:57 -0800

    Not sure whats happening anymore

--- studio1/script.js
+++ studio1/script.js
@@ -3,8 +3,9 @@ console.log('reading');
 // capture the submit event
 document.f.onsubmit = processForm;
 document.f.onreset = clearForm;
-console.log("youre outside of processForm")
+console.log("youre outside of processForm");
 // define process function
+
 function processForm() {
     console.log("you're inside processForm");
     // store form fields in variables
@@ -21,10 +22,11 @@ function processForm() {
     //print into message
     myMsg.innerHTML="Dear Professor, <br> On <u>" + day + "</u>, I was in a <u>" + adj1 + "</u> accident; that's why I was late to <u>" + className + "</u>. While I was <u>" + verb1 + "</u> to the <u>" + loca1 + "</u>, a <u>" + vehicle + "</u> knocked me off the ground, and I flew at least 2 feet in front of me. All of my homework scattered, and I saw a <u>" + animal + "</u> eat it. Then the <u>" + vehicle + "</u> also ran over my laptop. That's why I was really late and didn't have my homework. <br> Sorry, <u>" + yourName + "</u>";
     myMsg.className = "show";
-return false;
+    return false;
+
 }
 
 //clear clearForm
 function clearForm(){
-  myMsg.className = "hide";
+ myMsg.className = "hide";
 }
