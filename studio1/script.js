cdbfdcc77741d90bd9d3ab9913e9155844fe1268
emilyyylin <emwlin@ucdavis.edu>
2018-01-28 17:00:02 -0800

    Updated Design on Madlibs

--- studio1/script.js
+++ studio1/script.js
@@ -13,12 +13,13 @@ function processForm() {
     var className = document.f.className.value;
     var verb1 = document.f.verb1.value;
     var loca1 = document.f.loca1.value;
+    var animal = document.f.animal.value;
     var vehicle = document.f.vehicle.value;
   //  console.log(date_time);
     var myMsg=document.getElementById("myMsg");
 
     //print into message
-    myMsg.innerHTML="Dear Professor, <br> On <em>" + day + "</em>, I was in a <em>" + adj1 + "</em> accident; that's why I was late to <em>" + className + "</em>. While I was <em>" + verb1 + "</em> to the<em>" + loca1 + "</em> ,a <em>" + vehicle + "</em> knocked me off the ground, and I flew at least 2 feet in front of me.";
+    myMsg.innerHTML="Dear Professor, <br> On <u>" + day + "</u>, I was in a <u>" + adj1 + "</u> accident; that's why I was late to <u>" + className + "</u>. While I was <u>" + verb1 + "</u> to the <u>" + loca1 + "</u>, a <u>" + vehicle + "</u> knocked me off the ground, and I flew at least 2 feet in front of me. All of my homework scattered, and I saw a <u>" + animal + "</u> eat it. Then the <u>" + vehicle + "</u> also ran over my laptop. That's why I was really late and didn't have my homework.";
 
     myMsg.className = "show";
 
