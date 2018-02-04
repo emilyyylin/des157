f733a6ce712c5f66f95757a7bd515efde67ba5da
emilyyylin <emwlin@ucdavis.edu>
2018-02-03 19:12:30 -0800

    Hover Works

--- studio2/script.js
+++ studio2/script.js
@@ -11,6 +11,7 @@ var bottleH = document.getElementById('bottleH');
 var laptopH = document.getElementById('laptopH');
 var wasabiH = document.getElementById('wasabiH');
 
+//hover for bottle
 bottleH.addEventListener('mouseover', function () {
     bottleH.style.opacity = 1;
     bottleH.src = 'images/bottle.png';
@@ -19,4 +20,26 @@ bottleH.addEventListener('mouseover', function () {
 bottleH.addEventListener('mouseout', function () {
     bottleH.style.opacity = 1;
     bottleH.src = 'images/bottleH.png';
+});
+
+//hover for laptop
+laptopH.addEventListener('mouseover', function () {
+    laptopH.style.opacity = 1;
+    laptopH.src = 'images/laptop.png';
+});
+
+laptopH.addEventListener('mouseout', function () {
+    laptopH.style.opacity = 1;
+    laptopH.src = 'images/laptopH.png';
+});
+
+//hover for wasabi
+wasabiH.addEventListener('mouseover', function () {
+    wasabiH.style.opacity = 1;
+    wasabiH.src = 'images/wasabi.png';
+});
+
+wasabiH.addEventListener('mouseout', function () {
+    wasabiH.style.opacity = 1;
+    wasabiH.src = 'images/wasabiH.png';
 });
