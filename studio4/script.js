bc910317c7226d85f00c8646469e4f456c236006
emilyyylin <emwlin@ucdavis.edu>
2018-02-17 01:22:45 -0800

    Broken Af

--- studio4/script.js
+++ studio4/script.js
@@ -1,6 +1,28 @@
 "use strict";
 console.log("testing");
 
-$("img").click(function(){
+$("header img").click(function(){
 $  (this).fadeTo("slow", 0);
 });
+
+var sushiArray = [
+  {image: "s1.png"},
+  {image: "s2.png"},
+  {image: "s3.png"},
+  {image: "s4.png"},
+  {image: "s5.png"},
+  {image: "s6.png"},
+  {image: "s7.png"},
+  {image: "s8.png"},
+  {image: "s9.png"},
+  {image: "s10.png"},
+  {image: "s11.png"},
+  {image: "s12.png"},
+]
+
+var img=document.getElementsByTagName("img")[0];
+
+window.addEventListener('resize',function(){
+  var i = Math.floor(Math.random()*sushiArray.length);
+  imc.src=sushiArray[i];
+})
