bf1fbb814167434808eb2c7c25f7b87a490fce27
emilyyylin <emwlin@ucdavis.edu>
2018-02-16 23:44:07 -0800

    Fade

--- studio4/script.js
+++ studio4/script.js
@@ -1,13 +1,6 @@
 "use strict";
 console.log("testing");
 
-var layover = document.getElementByID('layover');
-
-layover.addEventListener('click', function(){
-  console.log(insideLayover);
-  if(layover.src=="https://emilyyylin.github.io/des157/studio4/images/layover.jpg"){
-    layover.src=="https://emilyyylin.github.io/des157/studio4/images/background.jpg";
-  } else {
-    layover.src=="https://emilyyylin.github.io/des157/studio4/images/layover.jpg";
-  }
+$("img").click(function(){
+$  (this).fadeTo("slow", 0);
 });
