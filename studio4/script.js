d72c245accd117637c30c6adc5a96465b3c87b19
emilyyylin <emwlin@ucdavis.edu>
2018-02-18 14:17:08 -0800

    Studio4 Update

--- studio4/script.js
+++ studio4/script.js
@@ -5,7 +5,7 @@ $("header img").click(function(){
 $  (this).fadeTo("slow", 0);
 });
 
-var sushiArray = [
+/*var sushiArray = [
   {image: "s1.png"},
   {image: "s2.png"},
   {image: "s3.png"},
@@ -25,4 +25,14 @@ var img=document.getElementsByTagName("img")[0];
 window.addEventListener('resize',function(){
   var i = Math.floor(Math.random()*sushiArray.length);
   imc.src=sushiArray[i];
-})
+})*/
+
+
+window.onload = choosePic;
+
+var sushiArray = new Array("images/s1.png","images/s2.png","images/s3.png","images/s4.png","images/s5.png","images/s6.png","images/s7.png","images/s8.png","images/s9.png","images/s10.png","images/s11.png","images/s12.png");
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * sushiArray.length);
+     document.getElementById("sushiTime").src = sushiArray[randomNum];
+   }
