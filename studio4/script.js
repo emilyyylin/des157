46fa50ec3836f35806ef4cd4986b8be182b3c06e
emilyyylin <emwlin@ucdavis.edu>
2018-02-19 09:48:01 -0800

    Sushi Update
    Added another array, click both

--- studio4/script.js
+++ studio4/script.js
@@ -1,9 +1,10 @@
 "use strict";
 console.log("testing");
 
-$("header img").click(function(){
+/*$("header img").click(function(){
 $  (this).fadeTo("slow", 0);
-});
+
+});*/
 
 /*var sushiArray = [
   {image: "s1.png"},
@@ -18,21 +19,21 @@ $  (this).fadeTo("slow", 0);
   {image: "s10.png"},
   {image: "s11.png"},
   {image: "s12.png"},
-]
-
-var img=document.getElementsByTagName("img")[0];
-
-window.addEventListener('resize',function(){
-  var i = Math.floor(Math.random()*sushiArray.length);
-  imc.src=sushiArray[i];
-})*/
-
+]*/
 
 window.onclick = choosePic;
 
-var sushiArray = new Array("images/s1.png","images/s2.png","images/s3.png","images/s4.png","images/s5.png","images/s6.png","images/s7.png","images/s8.png","images/s9.png","images/s10.png","images/s11.png","images/s12.png");
+var sushiArray1 = new Array("images/s1.png","images/s2.png","images/s3.png","images/s4.png","images/s5.png","images/s6.png");
+
+var sushiArray2 = new Array("images/s7.png","images/s8.png","images/s9.png","images/s10.png","images/s11.png","images/s12.png");
 
 function choosePic() {
-     var randomNum = Math.floor(Math.random() * sushiArray.length);
-     document.getElementById("sushiTime").src = sushiArray[randomNum];
+        console.log("inside choosePic");
+     var randomNum = Math.floor(Math.random() * sushiArray1.length);
+     document.getElementById("sushiTime").src = sushiArray1[randomNum];
+         console.log("sushiArray1 is working");
+
+     var randomNum2 = Math.floor(Math.random() * sushiArray2.length);
+     document.getElementById("sushiTime2").src = sushiArray2[randomNum2];
+         console.log("sushiArray2 is working");
    }
