b5c762679c851a47c9ee40566c6772a02351edac
emilyyylin <emwlin@ucdavis.edu>
2018-02-21 16:19:04 -0800

    Update

--- studio4/script.js
+++ studio4/script.js
@@ -1,21 +1,6 @@
 "use strict";
 console.log("testing");
 
-/*var sushiArray = [
-  {image: "s1.png"},
-  {image: "s2.png"},
-  {image: "s3.png"},
-  {image: "s4.png"},
-  {image: "s5.png"},
-  {image: "s6.png"},
-  {image: "s7.png"},
-  {image: "s8.png"},
-  {image: "s9.png"},
-  {image: "s10.png"},
-  {image: "s11.png"},
-  {image: "s12.png"},
-]*/
-
 window.onclick = choosePic;
 
 var sushiArray1 = new Array("images/s1.svg","images/s2.svg","images/s3.svg","images/s4.svg","images/s5.svg","images/s6.svg", "images/s7.svg","images/s8.svg","images/s9.svg","images/s10.svg","images/s11.svg","images/s12.svg");
