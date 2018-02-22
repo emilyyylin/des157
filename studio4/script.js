656c9917b1f85438cb41777dc18852b3712af462
emilyyylin <emwlin@ucdavis.edu>
2018-02-21 20:59:14 -0800

    What Happen

--- studio4/script.js
+++ studio4/script.js
@@ -1,6 +1,31 @@
 "use strict";
 console.log("testing");
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+// does same thing as the attribute ondrop="drop(event)"
+var div1 = document.querySelector("#div1");
+// div1.ondrop = drop;
+div1.addEventListener("drop", drop);
+div1.addEventListener("dragover", allowDrop);
+
+var drag1 = document.querySelector("#drag1");
+drag1.addEventListener("dragstart", drag);
+
+
+/*
 window.onclick = choosePic;
 
 var sushiArray1 = new Array("images/s1.svg","images/s2.svg","images/s3.svg","images/s4.svg","images/s5.svg","images/s6.svg", "images/s7.svg","images/s8.svg","images/s9.svg","images/s10.svg","images/s11.svg","images/s12.svg");
