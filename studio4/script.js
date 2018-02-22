b4ba6c07e65244734dd527f9d9cae8568cbc534b
emilyyylin <emwlin@ucdavis.edu>
2018-02-21 16:05:47 -0800

    Svg Upload

--- studio4/script.js
+++ studio4/script.js
@@ -18,9 +18,11 @@ console.log("testing");
 
 window.onclick = choosePic;
 
-var sushiArray1 = new Array("images/s1.png","images/s2.png","images/s3.png","images/s4.png","images/s5.png","images/s6.png");
+var sushiArray1 = new Array("images/s1.svg","images/s2.svg","images/s3.svg","images/s4.svg","images/s5.svg","images/s6.svg", "images/s7.svg","images/s8.svg","images/s9.svg","images/s10.svg","images/s11.svg","images/s12.svg");
 
-var sushiArray2 = new Array("images/s7.png","images/s8.png","images/s9.png","images/s10.png","images/s11.png","images/s12.png");
+var sushiArray2 = new Array("images/s7.svg","images/s8.svg","images/s9.svg","images/s10.svg","images/s11.svg","images/s12.svg");
+
+var sushiArray3 = new Array("images/s1.svg","images/s2.svg","images/s3.svg","images/s4.svg","images/s5.svg","images/s6.svg", "images/s7.svg","images/s8.svg","images/s9.svg","images/s10.svg","images/s11.svg","images/s12.svg");
 
 function choosePic() {
         console.log("inside choosePic");
@@ -31,4 +33,8 @@ function choosePic() {
      var randomNum2 = Math.floor(Math.random() * sushiArray2.length);
      document.getElementById("sushiTime2").src = sushiArray2[randomNum2];
          console.log("sushiArray2 is working");
-   }
+
+     var randomNum3 = Math.floor(Math.random() * sushiArray3.length);
+     document.getElementById("sushiTime3").src = sushiArray3[randomNum2];
+         console.log("sushiArray3 is working");
+   };
