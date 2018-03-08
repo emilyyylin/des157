c49184ca417a8bd577c28d10102e9a1a9a9cea18
emilyyylin <emwlin@ucdavis.edu>
2018-03-08 10:37:41 -0800

    fek

--- prototype2/script.js
+++ prototype2/script.js
@@ -2,8 +2,13 @@
 
 console.log('reading');
 
-window.alert("designed for iPhone7 view!!");
+//window.alert("designed for iPhone7 view!!");
 
 function myFunction(){
   alert("sorriez, I don't work yet :(");
+}
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
 }
