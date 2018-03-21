473fafc437b3ba688d70e35dccb084ba1f7a0bd4
emilyyylin <emwlin@ucdavis.edu>
2018-03-20 22:14:48 -0700

    UGH Y

--- final/script.js
+++ final/script.js
@@ -1,16 +1,17 @@
-(function () {
+"use strict";
 
-    // Setting up the options
-    var options = {
-        startLength: 0, // start positioning the slider items at the beginning of the SVG path
-        duration: 3000, // animation duration (used by anime.js)
-        stagger: 15, // incrementally delays among items, producing an staggering effect
-        easing: 'easeOutElastic', // easing function (used by anime.js)
-        elasticity: 600, // elasticity factor (used by anime.js)
-        rotate: true // This indicates that items should be rotated properly to match the SVG path curve
-    };
+console.log("hello!");
 
-    // Initialize the slider using our SVG path, items, and options
-    new PathSlider('.path-slider__path', '.path-slider__item', options);
+var salmon = document.getElementById('salmon');
+var tuna = document.getElementById('tuna');
 
-})();
+console.log("done with vars");
+
+//INGREDIENTS---------------------------------------------
+salmon.addEventListener("click", function(){
+  salmonInfo.style.display="block";
+});
+
+tuna.addEventListener('click', function(){
+  tunaInfo.style.display="block";
+})
