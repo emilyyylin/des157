5f1786aeda257df811734d65c70cfdb792628265
emilyyylin <emwlin@ucdavis.edu>
2018-03-21 20:09:12 -0700

    Why

--- final/script.js
+++ final/script.js
@@ -2,8 +2,27 @@
 
 console.log("hello!");
 
+//DECLARING VARIABLES-----------------------------------------------
+var nigiri = document.getElementById('nigiri');
+var nigiriClose = document.getElementById('nigiriClose')
+var sashimi = document.getElementById('sashimi');
+var maki = document.getElementById('maki');
+var uramaki = document.getElementById('uramaki');
+var temaki = document.getElementById('temaki');
+var sushiMain =document.getElementById('sushiMain');
+
+
 //INGREDIENTS--------------------------------------------------
 sushiMain.addEventListener('click', function(){
   ingredient.style.display="block";
   console.log("ingredients are displayed");
+});
+
+//NIGIRI INFO--------------------------------------------
+nigiri.addEventListener("click", function(){
+  nigiriInfo.style.display="block";
+});
+
+nigiriClose.addEventListener('click', function(){
+  nigiriInfo.style.display='none';
 });
