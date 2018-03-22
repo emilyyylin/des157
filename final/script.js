62648f58b75484ac76683f72bf526b834bd68812
emilyyylin <emwlin@ucdavis.edu>
2018-03-21 20:34:21 -0700

    why

--- final/script.js
+++ final/script.js
@@ -6,16 +6,25 @@ console.log("hello!");
 var nigiri = document.getElementById('nigiri');
 var nigiriClose = document.getElementById('nigiriClose')
 var sashimi = document.getElementById('sashimi');
+var sashimiClose = document.getElementById("sashimiClose");
 var maki = document.getElementById('maki');
+var makiClose = document.getElementById('makiClose');
 var uramaki = document.getElementById('uramaki');
+var uramakiClose = document.getElementById('uramakiClose');
 var temaki = document.getElementById('temaki');
-var sushiMain =document.getElementById('sushiMain');
+var temakiClose = document.getElementById('temakiClose');
 
+var sushiMain = document.getElementById('sushiMain');
+var close = document.getElementById("close");
 
 //INGREDIENTS--------------------------------------------------
-sushiMain.addEventListener('click', function(){
+sushiMain.addEventListener("click", function(){
   ingredient.style.display="block";
-  console.log("ingredients are displayed");
+    console.log("ingredients are displayed");
+});
+
+close.addEventListener("click", function(){
+  ingredient.style.display="none";
 });
 
 //NIGIRI INFO--------------------------------------------
@@ -25,4 +34,40 @@ nigiri.addEventListener("click", function(){
 
 nigiriClose.addEventListener('click', function(){
   nigiriInfo.style.display='none';
+});
+
+//SASHIMI INFO--------------------------------------------
+sashimi.addEventListener("click", function(){
+  sashimiInfo.style.display="block";
+});
+
+sashimiClose.addEventListener('click', function(){
+  sashimiInfo.style.display='none';
+});
+
+//MAKI INFO--------------------------------------------
+maki.addEventListener("click", function(){
+  makiInfo.style.display="block";
+});
+
+makiClose.addEventListener('click', function(){
+  makiInfo.style.display='none';
+});
+
+//URAMAKI INFO--------------------------------------------
+uramaki.addEventListener("click", function(){
+  uramakiInfo.style.display="block";
+});
+
+uramakiClose.addEventListener('click', function(){
+  uramakiInfo.style.display='none';
+});
+
+//TEMAKI INFO--------------------------------------------
+temaki.addEventListener("click", function(){
+  temakiInfo.style.display="block";
+});
+
+temakiClose.addEventListener('click', function(){
+  temakiInfo.style.display='none';
 });
